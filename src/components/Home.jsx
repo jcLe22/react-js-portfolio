d72c93a209a2e97c1dda3a2ffef518cbd91696ef
johnchristian.lebanto <johnchristian.lebanto@gmail.com>
2024-02-05 22:44:45 +0800

    added Socials Feature

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import jcImage from '../assets/jc-img.jpg';
 import { FaBootstrap, FaCss3Alt, FaHtml5, FaNodeJs, FaReact, FaSass } from 'react-icons/fa';
-import { SiExpress, SiJquery, SiMongodb, SiMysql, SiPostgresql, SiTailwindcss } from 'react-icons/si';
+import { SiExpress, SiJquery, SiMongodb, SiMysql, SiTailwindcss } from 'react-icons/si';
 import { DiJavascript } from 'react-icons/di';
 
 const feSkills = [
@@ -61,24 +61,26 @@ const beSkills = [
 const Home = () => {
   return (
     <div name='home' className='bg-gradient-to-b from-orange-50 to-slate-50'>
-    <div className='flex flex-col max-w-screen-lg mx-auto md:h-screen md:justify-center px-4 md:items-center'>
+    <div className='flex flex-col max-w-screen-lg mx-auto md:h-screen md:justify-center px-10 md:items-center'>
 
         {/* Introduction */}
-        <div className='flex flex-col md:flex-row gap-6 md:gap-20'>
+        <div className='flex flex-col md:flex-row gap-6'>
 
             <div className='md:order-2 pt-6 md:pt-0'>
                 <img src={jcImage} alt='admin-portrait' className='rounded-full border-dashed border-blue-300 border-8 w-80 md:w-full h-auto mx-auto'/>
             </div>
 
-            <div className='flex flex-col justify-center items-start md:order-1'>
-                <h2 className='text-3xl md:text-4xl mb-3 border-b-4 border-solid border-blue-300'>Hi, I am <span className='font-extrabold'>John Lebanto</span>.</h2>
-                <p>I'm a Junior Web Developer, proficient in creating web applications using MERN Stack. I continuously strive to enhance my skills in web design by studying modern techniques and technologies. I am open to learning different roles in the industry.</p>
+            <div className='flex flex-col justify-center items-center md:items-start md:order-1'>
+                <h2 className='text-3xl lg:text-4xl mb-3 border-b-4 border-solid border-blue-300'>
+                <span className='hidden md:inline'>Hi, I am </span>
+                <span className='font-extrabold'>John Lebanto</span></h2>
+                <p className='text-center md:text-left text-sm lg:text-lg'>I'm a Junior Web Developer, proficient in creating web applications using MERN Stack. I continuously strive to enhance my skills in web design by studying modern techniques and technologies. I am open to learning different roles in the industry.</p>
             </div>
 
         </div>
 
         {/* Tech Stack */}
-        <div className='flex flex-col md:flex-row items-center my-6 text-3xl md:text-5xl'>
+        <div className='flex flex-col lg:flex-row items-center my-6 text-3xl md:text-5xl'>
 
             <ul className='flex mb-3'>
                 {feSkills.map(({ id, feSkill }) => (
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { FaFileAlt, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { MdOutgoingMail } from 'react-icons/md';
 
 const Socials = () => {
+
+  const socLinks = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={20} />
+        </>
+      ),
+      href: 'https://www.linkedin.com/in/john-christian-lebanto-962bb6194/',
+      style: 'rounded-tl-sm'
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={20} />
+        </>
+      ),
+      href: 'https://github.com/jcLe22'
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Email <MdOutgoingMail size={20} />
+        </>
+      ),
+      href: 'mailto:johnchristian.lebanto@gmail.com'
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <FaFileAlt size={20} />
+        </>
+      ),
+      href: 'https://drive.google.com/file/d/1IhI6KdYUh50M03kliYDPBxkE0enFPrNS/view',
+      style: 'rounded-bl-sm'
+    }
+  ];
+
   return (
-    <div className='flex bottom-[15%] right-5 fixed'>
+    <div className='hidden lg:flex flex-col top-[45%] left-0 fixed'>
         <ul>
-            <li>
-                <a></a>
-            </li>
+
+            {socLinks.map(({ id, child, href, style }) => (
+
+              <li key={id} className={`flex justify-between items-center w-28 h-9 px-2 py-1 ml-[-80px] hover:ml-0 hover:rounded-sm duration-300 bg-blue-300 ${style}`}>
+
+                <a href={href} className='flex justify-between items-center w-full font-bold' target='_blank' rel='noreferrer'>
+                  {child}
+                </a>
+
+              </li>
+
+            ))}
+
+            
         </ul>
     </div>
   )
